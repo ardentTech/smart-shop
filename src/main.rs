9717e0eeb857482241e038a6879490f476370ae4
Jonathan D. Baker <jonathan@ardent.tech>
2025-06-13 12:00:06 -0600

    integrate packed_struct lib for lora tx payload

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,11 @@ use embassy_rp::peripherals::{I2C1, USB};
 use embassy_rp::spi::Spi;
 use embassy_rp::usb::Driver;
 use embassy_sync::mutex::Mutex;
-use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
-use embassy_sync::channel::{Channel, Sender};
 use embassy_time::{Delay, Timer};
 use embedded_sht3x::{Sht3x, DEFAULT_I2C_ADDRESS};
+use packed_struct::prelude::*;
 use panic_halt as _;
-use pmsa003i::{Pmsa003i, Reading};
+use pmsa003i::Pmsa003i;
 use static_cell::StaticCell;
 use crate::board::Board;
 use crate::radio::Radio;
@@ -49,21 +48,19 @@ impl TemperatureHumidityReading {
     }
 }
 
-pub enum Event {
-    // TODO wrap AQ Err and Reading as needed: github.com/bbustin/pmsa003i/blob/master/src/error.rs
-    // need to wrap pmsa003i return value bc it uses generics and embassy async tasks cannot
-    AqReadErr,
-    AqReadOk(Reading),
-    Nop,
-    TempHumidityReadErr,
-    // TODO add data to temp humidity
-    TempHumidityReadOk
+#[derive(PackedStruct)]
+#[packed_struct(endian="lsb")]
+struct ShopReading {
+    #[packed_field()]
+    aq_pm2_5: u16,
+    #[packed_field()]
+    aq_pm10: u16,
+    #[packed_field()]
+    humidity: u16,
+    #[packed_field()]
+    temperature: u16,
 }
-
-const EVENT_BUS_BUFFER_SIZE: usize = 64;
 const READ_INTERVAL_SECONDS: u64 = 3;
-
-static EVENT_BUS: Channel<ThreadModeRawMutex, Event, EVENT_BUS_BUFFER_SIZE> = Channel::new();
 
 bind_interrupts!(struct Irqs {
     I2C1_IRQ => i2c::InterruptHandler<I2C1>;
@@ -90,36 +87,6 @@ async fn air_quality(
 }
 
 #[embassy_executor::task]
-async fn event_handler(radio: &'static LoRaRadio) {
-    loop {
-        match EVENT_BUS.receive().await {
-            Event::AqReadOk(_data) => {
-                // TODO transform data to [u8]
-                let data = [0, 1, 0, 1, 0, 1];
-                let mut radio = radio.lock().await;
-                log::info!("aq ok: {:?}", &data);
-                radio.tx(&data).await.unwrap();
-                log::info!("lora tx ok")
-            }
-            Event::AqReadErr => {
-                // TODO lora tx?
-            }
-            Event::Nop => {}
-            Event::TempHumidityReadErr => {
-                // TODO lora tx?
-            }
-            Event::TempHumidityReadOk => {
-                let data = [1, 0, 1, 0, 1, 0];
-                let mut radio = radio.lock().await;
-                log::info!("temp humidity ok: {:?}", &data);
-                radio.tx(&data).await.unwrap();
-                log::info!("lora tx ok")
-            }
-        }
-    }
-}
-
-#[embassy_executor::task]
 async fn logger(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
 }
@@ -127,21 +94,26 @@ async fn logger(driver: Driver<'static, USB>) {
 #[embassy_executor::task]
 async fn env_sensors(
     i2c_bus: &'static I2c1Bus,
-    event_bus: Sender<'static, ThreadModeRawMutex, Event, EVENT_BUS_BUFFER_SIZE>
+    //event_bus: Sender<'static, ThreadModeRawMutex, Event, EVENT_BUS_BUFFER_SIZE>,
+    radio: &'static LoRaRadio,
 ) {
     match join(
         air_quality(i2c_bus),
         temp_humidity(i2c_bus)
     ).await {
         (Ok(aq), Ok(th)) => {
-            log::info!("join succeeded :)");
-            log::info!("aq: {:?}", aq);
-            log::info!("th: {:?}", th);
-            let mut bytes: [u8; 4] = aq.aq_pm10.to_le_bytes().iter().chain(&th).collect();
-            //let bytes: [u8; 4] = [aq.aq_pm2_5.to_le_bytes(), aq.aq_pm10.to_le_bytes()].concat();
-            // radio_tx(...).await
+            let reading = ShopReading {
+                aq_pm2_5: aq.aq_pm2_5.into(),
+                aq_pm10: aq.aq_pm10.into(),
+                humidity: (th.humidity as u16).into(),
+                temperature: (th.temperature as u16).into(),
+            };
+            let payload: [u8; 8] = reading.pack().unwrap();
+            radio_tx(radio, &payload).await;
+            log::info!("radio tx succeeded with: {:?}", payload)
         },
         _ => {
+            // TODO handle partial and total failures
             log::info!("join failed :(");
         }
     }
@@ -188,10 +160,9 @@ async fn main(spawner: Spawner) {
     );
     static SPI_BUS: StaticCell<Spi1Bus> = StaticCell::new();
     let spi_bus = SPI_BUS.init(Mutex::new(spi));
+
     static RADIO: StaticCell<LoRaRadio> = StaticCell::new();
     let radio = RADIO.init(Mutex::new(Radio::new(spi_bus, board.lora.nss, board.lora.reset, board.lora.dio0).await));
-
-    spawner.must_spawn(event_handler(radio));
 
     // TODO handle this config in Board?
     // defaults to 100 kbps, which is the only speed the AQ sensor works with
@@ -206,7 +177,7 @@ async fn main(spawner: Spawner) {
     let i2c_bus = I2C_BUS.init(Mutex::new(i2c));
 
     loop {
-        spawner.must_spawn(env_sensors(i2c_bus, EVENT_BUS.sender()));
+        spawner.must_spawn(env_sensors(i2c_bus, radio));
         Timer::after_secs(READ_INTERVAL_SECONDS).await;
     }
 }
