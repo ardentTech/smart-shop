1c091c4422af3703d02f0ef552fc98a8eb863bd3
Jonathan D. Baker <jonathan@ardent.tech>
2025-06-25 10:00:45 -0600

    add air quality unit tests

--- air_quality/src/lib.rs
+++ air_quality/src/lib.rs
@@ -1,7 +1,24 @@
 #![no_std]
 
 use embedded_hal_async::i2c::I2c;
-use pmsa003i::Pmsa003i;
+use pmsa003i::{Error, Pmsa003i};
+
+#[derive(Debug, PartialEq)]
+pub enum AirQualityError<E> {
+    I2C(E),
+    InvalidChecksum,
+    InvalidMagic
+}
+
+impl<E> From<Error<E>> for AirQualityError<E> {
+    fn from(e: Error<E>) -> Self {
+        match e {
+            Error::I2C(e) => AirQualityError::I2C(e),
+            Error::BadChecksum => AirQualityError::InvalidChecksum,
+            Error::BadMagic => AirQualityError::InvalidMagic
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct AirQualityReading {
@@ -24,26 +41,82 @@ impl<I2C: I2c> AQSensor<I2C> {
         Self { i2c }
     }
 
-    pub async fn read(&mut self) -> Result<AirQualityReading, ()> {
+    pub async fn read(&mut self) -> Result<AirQualityReading, AirQualityError<I2C::Error>> {
         let mut sensor = Pmsa003i::new(&mut self.i2c);
-
-        match sensor.read().await {
-            Ok(data) => {
-                Ok(AirQualityReading::new(data.pm2_5, data.pm10))
-            },
-            // TODO wrap errors
-            _ => {
-                Err(())
-            },
-        }
+        let data = sensor.read().await.map_err(AirQualityError::from)?;
+        Ok(AirQualityReading::new(data.pm2_5, data.pm10))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::*;
+    use embedded_hal_async::i2c::ErrorKind;
+    use embedded_hal_mock::eh1::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
 
-    #[test]
-    fn it_works() {
+    const ADDR: u8 = 0x12;
+    const RESPONSE_LEN: usize = 32;
+
+    #[tokio::test]
+    async fn read_i2c_error() {
+        let expectations = [
+            I2cTransaction::read(ADDR, [0u8; RESPONSE_LEN].to_vec()).with_error(ErrorKind::Other)
+        ];
+        let mut i2c = I2cMock::new(&expectations);
+        let mut sensor = AQSensor::new(&mut i2c);
+        let err = sensor.read().await.unwrap_err();
+        assert_eq!(err, AirQualityError::I2C(ErrorKind::Other));
+        i2c.done();
+    }
+
+    #[tokio::test]
+    async fn read_invalid_magic() {
+        let mut res = get_valid_response();
+        res[0] = 0x00;
+        let expectations = [
+            I2cTransaction::read(ADDR, res.to_vec())
+        ];
+        let mut i2c = I2cMock::new(&expectations);
+        let mut sensor = AQSensor::new(&mut i2c);
+        let err = sensor.read().await.unwrap_err();
+        assert_eq!(err, AirQualityError::InvalidMagic);
+        i2c.done();
+    }
+
+    #[tokio::test]
+    async fn read_invalid_checksum() {
+        let mut res = get_valid_response();
+        res[RESPONSE_LEN - 1] = 0x00;
+        let expectations = [
+            I2cTransaction::read(ADDR, res.to_vec())
+        ];
+        let mut i2c = I2cMock::new(&expectations);
+        let mut sensor = AQSensor::new(&mut i2c);
+        let err = sensor.read().await.unwrap_err();
+        assert_eq!(err, AirQualityError::InvalidChecksum);
+        i2c.done();
+    }
+
+    #[tokio::test]
+    async fn read_ok() {
+        let res = get_valid_response();
+        let expectations = [
+            I2cTransaction::read(ADDR, res.to_vec())
+        ];
+        let mut i2c = I2cMock::new(&expectations);
+        let mut sensor = AQSensor::new(&mut i2c);
+        sensor.read().await.unwrap();
+        i2c.done();
+    }
+
+    fn get_valid_response() -> [u8; RESPONSE_LEN] {
+        let mut res = [0x00; RESPONSE_LEN];
+        // valid start of frame
+        res[0] = 0x42;
+        res[1] = 0x4d;
+        // valid checksum
+        res[30] = 0x00;
+        res[31] = 0x8f;
+        res
     }
 }
